9c5bb3cddf514aa9fa4b6b0c2b69033a26e570b8
undersourcecode <undersourcecode@hotmail.co.jp>
2020-01-04 14:20:27 +0900

    add stage.

--- README.md
+++ README.md
@@ -13,15 +13,17 @@ Before run, prepare below.
 * ~/.aws/credentials
     * access key, secret key, region
 * EC2 Keypair
-* ~/.config/environments.ts
+* .config/config-dev.json
+    * $ cp .config/config-sample.json .config/config-dev.json
     * ami ・・・ your favorite ec2 ami id
     * keyName ・・・ your Keypair name
 
 ## Useful commands
 
- * `npm run build`   compile typescript to js
- * `npm run watch`   watch for changes and compile
- * `npm run test`    perform the jest unit tests
- * `npm run deploy`  deploy this stack to your default AWS account/region
- * `npm run diff`    compare deployed stack with current state
- * `npm run synth`   emits the synthesized CloudFormation template
+ * `npm run build`                  compile typescript to js
+ * `npm run watch`                  watch for changes and compile
+ * `npm run test`                   perform the jest unit tests
+ * `npx cdk deploy -c stage=dev`    deploy this stack to your default AWS account/region
+ * `npx cdk diff -c stage=dev`      compare deployed stack with current state
+ * `npx cdk synth -c stage=dev`     emits the synthesized CloudFormation template
+ * `npx cdk destroy -c stage=dev`   remove all
--- lib/cdk-vpc-ec2-stack.ts
+++ lib/cdk-vpc-ec2-stack.ts
@@ -1,12 +1,15 @@
 import cdk = require('@aws-cdk/core');
 import ec2 = require('@aws-cdk/aws-ec2')
-import { environments as envs } from '../.config/environments'
+import fs = require('fs');
 
 export class CdkVpcEc2Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    const stage: string = this.node.tryGetContext("stage") ? this.node.tryGetContext("stage") : 'dev';
+    const config = JSON.parse(fs.readFileSync(`.config/config-${stage}.json`, {encoding: 'utf-8'}));
 
     const vpc = new ec2.Vpc(this, 'myVpc', {
       cidr: '10.0.0.0/16',
@@ -19,13 +22,14 @@ export class CdkVpcEc2Stack extends cdk.Stack {
     })
 
     const instance = new ec2.CfnInstance(this, 'myEC2', {
-      imageId: envs.ami,
+      imageId: config.ami,
       instanceType: 't2.micro',
-      keyName: envs.keyName,
+      keyName: config.keyName,
       subnetId: vpc.publicSubnets[0].subnetId,
       securityGroupIds: [securityGroup.securityGroupId]
     })
 
+    new cdk.CfnOutput(this, 'stage', { value: stage })
     new cdk.CfnOutput(this, 'VPC', { value: vpc.vpcId })
     new cdk.CfnOutput(this, 'Security Group', { value: securityGroup.securityGroupId })
     new cdk.CfnOutput(this, 'EC2 PublicIP', { value: instance.attrPublicIp })
